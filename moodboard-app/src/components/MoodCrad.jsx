ce7f2a5589cee4997e862b93c77a43b6362bcf3e
Aswabnth.v <aswanthv695@gmail.com>
2025-07-25 05:07:17 -0700

    made change

--- moodboard-app/src/components/MoodCrad.jsx
+++ moodboard-app/src/components/MoodCrad.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MoodCard = ({ emoji, mood, timestamp, note }) => {
+  const [card, setCard] = useState(true);
+
+  if (!card) return null;  
+
   return (
-    <div className="bg-white dark:bg-gray-700 p-4 rounded-lg shadow">
+    <div className="bg-white dark:bg-gray-700 p-8 rounded-lg shadow ">
+    <div className='flex justify-end'>  <button onClick={() => setCard(false)} >x</button></div>
       <div className="text-3xl">{emoji}</div>
       <h3 className="text-lg font-bold mt-2">{mood}</h3>
       <p className="text-sm text-gray-500">{new Date(timestamp).toLocaleString()}</p>
       {note && (
         <p className="mt-2 text-gray-800 dark:text-gray-100">
-          <strong>Note:</strong> {note}
+          <strong>Situation:</strong> {note}
         </p>
       )}
     </div>
